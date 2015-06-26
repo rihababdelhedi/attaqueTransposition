f99c47e5601092e78a3b649c69d4af4bc5e70be5
rihababdelhedi <rihab.abde@gmail.com>
2015-06-26 14:41:40 +0200

    attaque

---
+++ src/attaque/AttaqueTransposition.java
@@ -0,0 +1,11 @@
+package attaque;
+
+public class AttaqueTransposition {
+
+	private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	public int keylength(String message)
+	{
+		return 0;
+	}
+}
